d96c5057cbd3847891834c0f028e24982f5ce796
git <tdnorbra@nps.edu>
2025-03-04 22:36:53 -0800

    [Terry N.] new unit test

---
+++ test/unit/java/com/articulate/sigma/UnitTestBase.java
@@ -0,0 +1,69 @@
+package com.articulate.sigma;
+
+import com.articulate.sigma.trans.SUMOKBtoTPTPKB;
+
+import java.io.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Base class for fast-running true unit tests.
+ */
+public class UnitTestBase extends SigmaTestBase {
+
+    private static final String SIGMA_SRC = System.getenv("SIGMA_SRC");
+    public static final String CONFIG_FILE_DIR = SIGMA_SRC + File.separator +
+            "test/unit/java/resources";
+    private static final String CONFIG_FILE_PATH = CONFIG_FILE_DIR + File.separator +
+            "config_topOnly.xml";
+//    private static final String CONFIG_FILE_PATH = System.getenv("SIGMA_HOME") + "/KBs/config.xml";
+    private static final Class<?> CLASS = UnitTestBase.class;
+    public static final int NUM_KIF_FILES = 3; // config_topOnly.xml
+//    public static final int NUM_KIF_FILES = 45; // full config.xml
+
+    static Long totalKbMgrInitTime = Long.MAX_VALUE;
+
+    /***************************************************************
+     * Write out a meaningful error message if the config file path is bad.
+     * */
+    private static final Reader xmlReader;
+
+    static  {
+        xmlReader = SigmaTestBase.getXmlReader(CONFIG_FILE_PATH, CLASS);
+    }
+
+    /***************************************************************
+     * */
+    @BeforeClass
+    public static void setup()  {
+
+        System.out.println("UnitTestSuite.startUp(): SUMOKBtoTPTPKB.rapidParsing==" + SUMOKBtoTPTPKB.rapidParsing);
+
+        System.out.println("UnitTestBase.setup(): reading test config file: " + CONFIG_FILE_PATH);
+        System.out.println("***** UnitTestBase.setup(): warning! Note that only KB files in the test config file will be loaded! ***** ");
+//        System.out.println("***** UnitTestBase.setup(): Note that all KB files will be loaded! ***** "); // full config.xml
+        long startTime = System.currentTimeMillis();
+        SigmaTestBase.doSetUp(xmlReader);
+        long endTime = System.currentTimeMillis();
+        // Update the init time only if it has its initialized value.
+        if (UnitTestBase.totalKbMgrInitTime == Long.MAX_VALUE) {
+            UnitTestBase.totalKbMgrInitTime = endTime - startTime;
+        }
+    }
+
+    /***************************************************************
+     * */
+    @AfterClass
+    public static void checkKBCount() {
+
+        if (KBmanager.getMgr().getKB(KBmanager.getMgr().getPref("sumokbname")).constituents.size() > NUM_KIF_FILES) { // include cache file
+            System.out.println("FAILURE: This test is running with the wrong configuration. Please investigate immediately, since the problem does not consistently appear.");
+            System.out.println("  Because this test is changing the configuration, other tests may fail, even if this one passes.");
+            System.out.println("  Nbr kif files: " + KBmanager.getMgr().getKB(KBmanager.getMgr().getPref("sumokbname")).constituents.size());
+            fail();
+        }
+    }
+}
