67e4a61fae1cdd89c796bbba7ae155619bb733ed
git <tdnorbra@nps.edu>
2025-03-25 19:54:20 -0700

    [Terry N.] javadoc here

--- src/com/articulate/sigma/jedit/SUMOjEditActions.java
+++ src/com/articulate/sigma/jedit/SUMOjEditActions.java
@@ -20,36 +20,86 @@ package com.articulate.sigma.jedit;
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
+/** Defines the actions to take in this plugin */
 interface SUMOjEditActions {
-	//void chooseFile();
 
-	//void addHeader();
+    /**
+     * ***************************************************************
+     * Show statistics for a given buffer
+     */
+    void showStats();
 
-	void showStats();
+    /**
+     * ***************************************************************
+     * Check for a variety of syntactic and semantic errors and warnings in a
+     * given buffer
+     */
+    void checkErrors();
 
-	void checkErrors();
+    /**
+     * ***************************************************************
+     * Reformat a selection of SUO-KIF axioms in a buffer. In case of error,
+     * such as a selection that only spans part of an axiom, do nothing.
+     */
+    void formatSelect();
 
-	void formatSelect();
+    /**
+     * ***************************************************************
+     * Go to the "definition" of a selected term. If no term is selected do
+     * nothing other than print an WARN to the console. If definition is in
+     * another file, load that file.
+     */
+    void gotoDefn();
 
-	void gotoDefn();
+    /**
+     * ***************************************************************
+     * Open up a browser on the public Sigma for the highlighted term. If it's
+     * not a term, or not in the KB, don't open
+     */
+    void browseTerm();
 
-	void browseTerm();
+    /**
+     * ***************************************************************
+     * Send a highlighted expression as a query to a theorem prover. return
+     * results in a new tab
+     */
+    void queryExp();
 
-	void queryExp();
+    /**
+     * ***************************************************************
+     * Set theorem proving to use Vampire
+     */
+    void chooseVamp();
 
-	void chooseVamp();
+    /**
+     * ***************************************************************
+     * Set theorem proving to use E
+     */
+    void chooseE();
 
-	void chooseE();
+    /**
+     * ***************************************************************
+     * Set theorem proving to use FOF translation of SUMO
+     */
+    void setFOF();
 
-	void setFOF();
+    /**
+     * ***************************************************************
+     * Set theorem proving to use TFF translation of SUMO
+     */
+    void setTFF();
 
-	void setTFF();
+    /**
+     * ***************************************************************
+     * Convert a buffer or selection from SUO-KIF to TPTP. Note that this does
+     * not do full pre-processing, just a syntax translation
+     */
+    void toTPTP();
 
-	void toTPTP();
-
-	void fromTPTP();
-
-	//void saveFile();
-
-	//void copyToBuffer();
+    /**
+     * ***************************************************************
+     * Convert a buffer or selection from TPTP to SUO-KIF. Note that this does
+     * not do full pre-processing, just a syntax translation
+     */
+    void fromTPTP();
 }
