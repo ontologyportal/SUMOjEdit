82a4472601f8c7b9853860df9e71cdd2ef948b71
git <tdnorbra@nps.edu>
2025-07-18 16:26:35 -0700

    [Terry N.] command line invocation for checking syntax on a single *.kif file or directory of *.kif files

--- test/integration/java/com/articulate/sigma/parsing/SUMOParseTest.java
+++ test/integration/java/com/articulate/sigma/parsing/SUMOParseTest.java
@@ -8,8 +8,13 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
-import org.junit.Test;
 import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Request;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
 
 /** Go through the entire SUMO KB and parse, checking for syntax errors
  *
@@ -17,7 +22,11 @@ import static org.junit.Assert.*;
  */
 public class SUMOParseTest {
 
+    /** Relative path to a specific individual *.kif file */
+    static String relPath;
+
     private void parsePath(String path) throws IOException {
+
         Path inPath = Paths.get(path);
         try (Stream<Path> paths = Files.walk(inPath)) {
             paths.filter(f -> f.toString().endsWith(".kif")).sorted().forEach(f ->  {
@@ -38,6 +47,7 @@ public class SUMOParseTest {
 
     @Test
     public void test_sumo_kbs() {
+
         try {
             parsePath(System.getenv("ONTOLOGYPORTAL_GIT") + "/sumo");
         } catch (IOException ex) {
@@ -47,11 +57,41 @@ public class SUMOParseTest {
 
     @Test
     public void test_individual_kb() {
+
         try {
-            parsePath(System.getenv("ONTOLOGYPORTAL_GIT") + "/sumo/Geography.kif");
+            if (relPath == null)
+                parsePath(System.getenv("ONTOLOGYPORTAL_GIT") + "/sumo/Geography.kif");
+            else
+                parsePath(relPath);
         } catch (IOException ex) {
             fail(ex.getMessage());
         }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        if (args != null && !args[0].isEmpty())
+            relPath = args[0]; // $ONTOLOGYPORTAL_GIT/sumo
+        else {
+            System.err.println("Usage: ant -Dtest.file={full path to a *.kif file or directory of *.kif files} test.kif");
+            return;
+        }
+
+        Request request = Request.method(SUMOParseTest.class, "test_individual_kb");
+        Result result = new JUnitCore().run(request);
+
+        for (Failure failure : result.getFailures())
+            System.err.println(failure.toString());
+
+        boolean success = result.wasSuccessful();
+
+        if (success)
+            System.out.println(SUMOParseTest.class.getName() + "#test_individual_kb " + relPath  + " success: " + success);
+        else
+            System.err.println(SUMOParseTest.class.getName() + "$test_individual_kb " + relPath  + " failure: " + !success);
     }
 
 } // end class file SUMOParseTest.java
