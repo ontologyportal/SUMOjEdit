ee04555728da6a66eccad9696f4d15ab634543a3
sdeng99 <simon666699@hotmail.com>
2025-08-15 11:10:07 -0700

    Test commit message

--- src/com/articulate/sigma/jedit/SUMOjEditPlugin.java
+++ src/com/articulate/sigma/jedit/SUMOjEditPlugin.java
@@ -4,7 +4,7 @@ package com.articulate.sigma.jedit;
  * SUMOjEditPlugin.java
  * part of the SUMOjEdit plugin for the jEdit text editor
  * Copyright (C) 2019 Infosys
- * haha
+ * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
